a898be49b6ccff8b7798f119d21ac2181cf57ce4
Sergej Kaldesić <120308990+Kaldesic@users.noreply.github.com>
2026-08-20 23:47:16 +0200

    Update color-extractor.js

--- js/tools/color-extractor.js
+++ js/tools/color-extractor.js
@@ -157,11 +157,5 @@ window.Atelier.setPasteHandler((e) => {
             });
         }
 
-        if ('serviceWorker' in navigator) {
-            window.addEventListener('load', () => {
-                navigator.serviceWorker.register('../sw.js')
-                    .then(reg => console.log('PWA Ready:', reg.scope))
-                    .catch(err => console.error('SW registration failed:', err));
-            });
-        }
+        
 }
