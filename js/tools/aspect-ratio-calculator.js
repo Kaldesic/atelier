79a14d683c32106e91e4c0dc72bab3fb24f41a2c
Sergej Kaldesić <120308990+Kaldesic@users.noreply.github.com>
2026-08-20 23:46:57 +0200

    Update aspect-ratio-calculator.js

--- js/tools/aspect-ratio-calculator.js
+++ js/tools/aspect-ratio-calculator.js
@@ -133,11 +133,5 @@ const rW = document.getElementById('rW');
 
         calculate();
 
-        if ('serviceWorker' in navigator) {
-            window.addEventListener('load', () => {
-                navigator.serviceWorker.register('../sw.js')
-                    .then(reg => console.log('PWA Ready:', reg.scope))
-                    .catch(err => console.error('SW registration failed:', err));
-            });
-        }
+        
 }
